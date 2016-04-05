aba07ad5acc4149d43e17e8fcdae637942965980
Josh <engelsjo@mail.gvsu.edu>
2016-04-04 21:47:15 -0400

    added some view code

--- src/neural_network_view.py
+++ src/neural_network_view.py
@@ -6,13 +6,33 @@ from itertools import izip
 from numpy.random import randint
 from basicNN import NeuralNetwork
 import sys
+from graph_tool.all import *
 
 # create our Neural Network Model
 dataFile = sys.argv[1]
-neural_net = NeuralNetwork(dataFile)
+neural_net = NeuralNetwork(dataFile) 
 neural_net.initialMatrixWeights()
-neural_net.train()
+neural_net.trainAndTestNetwork()
 
 # create our graph
 g = Graph()
-g.add_vertex(100)
+
+# create the input nodes of our graph
+inputs = []
+nbrOfInputs = len(neural_net.targetValues[0])
+for i in range(nbrOfInputs):
+    inputs.append(g.add_vertex())
+
+# create the hidden nodes of our graph
+hidden_units = []
+for i in range(neural_net.numHiddenUnits):
+    hidden_units.append(g.add_vertex())
+
+# create the output nodes of our graph
+outputs = []
+nbrOfOutputs = len(neural_net.inputValues[0])
+for i in range(nbrOfOutputs):
+    outputs.append(g.add_vertex())
+
+# properly assign the edge weights
+
